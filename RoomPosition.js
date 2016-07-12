319c680c50ec723fe5fa028aa82b9857ec1503a6
Derek Reese <reesederek90@gmail.com>
2016-07-11 21:54:14 -0400

    Fixes free terrain to only ignore walls

--- RoomPosition.js
+++ RoomPosition.js
@@ -4,7 +4,7 @@ RoomPosition.prototype.getOpenTerrain = function(spaces) {
   for (var x = -spaces; x <= spaces; x++) {
     for (var y = -spaces; y <= spaces; y++) {
       var terrain_type = Game.map.getTerrainAt(this.x + x, this.y + y, this.roomName);
-      if (terrain_type == 'plain') {
+      if (terrain_type != 'wall') {
         terrain.push(new RoomPosition(x, y, this.roomName));
       }
     }
