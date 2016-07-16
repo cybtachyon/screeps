07e001c607a5d9f40801ca78aac78b9942c8a317
Derek Reese <reesederek90@gmail.com>
2016-07-16 00:05:24 -0400

    Fixes RoomPosition return position x y

--- RoomPosition.js
+++ RoomPosition.js
@@ -5,7 +5,7 @@ RoomPosition.prototype.getOpenTerrain = function(spaces) {
     for (var y = -spaces; y <= spaces; y++) {
       var terrain_type = Game.map.getTerrainAt(this.x + x, this.y + y, this.roomName);
       if (terrain_type != 'wall') {
-        terrain.push(new RoomPosition(x, y, this.roomName));
+        terrain.push(new RoomPosition(this.x + x, this.y + y, this.roomName));
       }
     }
   }
